03c82593e37675edd2769aee530c7b67ccd57695
Dharanidhar Reddy <dharanidhar.reddy@hashedin.com>
2018-12-25 22:06:37 +0530

    Express JS with hbs templating engine

---
+++ node-express/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+const app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerHelper("getCurrentYear", () => new Date().getFullYear());
+hbs.registerHelper("upperCase", text => text.toUpperCase());
+app.set("view engine", hbs);
+
+app.use((req, res, next) => {
+  const log = `${new Date().toString()} ${req.method} ${req.path} \n`;
+  fs.appendFile("server.log", `${log}`, err => {
+    if (err) {
+      console.log("Unable to log into file");
+    }
+    console.log(log);
+    next();
+  });
+});
+
+app.use((req, res, next) => {
+  res.render("maintainence.hbs");
+});
+
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  //   res.send("Hello Express!");
+  res.send({
+    name: "First Route",
+    context: "Hello Express!"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    title: "Page Title",
+    pageTitle: "Some Text"
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res
+    .send({
+      badRequest: "Error 404 Bad Request"
+    })
+    .status(400);
+});
+
+app.listen(3000, () => {
+  console.log("server is up on port 3000");
+});
